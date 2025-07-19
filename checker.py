e65eba5c6b73356c7d644072dababf608c13de87
Alcopony <88287711+Alcopony@users.noreply.github.com>
2025-07-19 22:19:19 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 CITIES = {
     "Гори": "https://api-my.sa.gov.ge/api/v1/DrivingLicensePracticalExams2/DrivingLicenseExamsDates2?CategoryCode=4&CenterId=7",
-    "Батуми": "https://api-my.sa.gov.ge/api/v1/DrivingLicensePracticalExams2/DrivingLicenseExamsDates2?CategoryCode=4&CenterId=5",
+    "Батуми": "https://api-my.sa.gov.ge/api/v1/DrivingLicensePracticalExams2/DrivingLicenseExamsDates2?CategoryCode=4&CenterId=3",
 }
 
 previous_slots = {city: set() for city in CITIES}
