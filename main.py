251522dbd3f181a7ec7f8317ac14221fa8f81444
Alcopony <88287711+Alcopony@users.noreply.github.com>
2025-07-19 22:11:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,45 @@
 import asyncio
 import logging
+import os
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.client.default import DefaultBotProperties
 from dotenv import load_dotenv
-import os
 
 from database import init_db, add_user, get_all_users
-from checker import check_for_new_slots
+from checker import check_for_new_slots, get_initial_slots
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=os.getenv("BOT_TOKEN"),
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     await add_user(message.from_user.id)
-    await message.answer("Вы подписались на уведомления о новых окнах для экзамена на ВУ!")
+    await message.answer("Пони подписано!")
 
 async def send_to_all_users(text):
     users = await get_all_users()
     for uid in users:
         try:
-            await bot.send_message(uid, f"📅 Появились новые слоты!\n\n{text}")
+            await bot.send_message(uid, f"📅 {text}")
         except Exception as e:
-            print(f"Ошибка отправки {uid}: {e}")
+            print(f"Не удалось отправить сообщение пользователю {uid}: {e}")
 
 async def on_startup():
     await init_db()
-    
-    # Сразу отправим текущие слоты всем пользователям
     initial_data = await get_initial_slots()
     if initial_data:
-        await send_to_all_users(f"📅 Актуальные доступные даты:\n\n{initial_data}")
-    
-    # Старт мониторинга
+        await send_to_all_users(f"Актуальные слоты:\n\n{initial_data}")
     asyncio.create_task(check_for_new_slots(send_to_all_users))
+
 if __name__ == "__main__":
     dp.startup.register(on_startup)
     dp.run_polling(bot)
